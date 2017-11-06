6fb0538d1fe22b28eb4acb8defd06b492404459a
Jordan Hudgens <jordan@wow.com>
2017-11-06 10:03:22 -0700

    Added more examples for decimal guide

--- D_Numbers/04_decimal.py
+++ D_Numbers/04_decimal.py
@@ -1,5 +1,24 @@
 from decimal import Decimal
+import decimal
 
-sale_commission = Decimal(0.3) / Decimal(100)
+total = Decimal(5010.42)
+sale_commission = Decimal(0.3) * total
+penny = Decimal('0.01')
 
+print("Standard Decimal Computation")
 print(sale_commission)
+
+print("--------")
+
+print("Quantize Decimal Computation")
+print(sale_commission.quantize(penny))
+
+print("--------")
+
+print("Quantize Decimal Computation Rounded Up")
+print(sale_commission.quantize(penny, decimal.ROUND_UP))
+
+print("--------")
+
+print("Quantize Decimal Computation Rounded Down")
+print(sale_commission.quantize(penny, decimal.ROUND_DOWN))
